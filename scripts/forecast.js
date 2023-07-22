146048e9d4ac142d025f822d19fe3d48c0fea6fe
Timisalam <102313891+Timisalam@users.noreply.github.com>
2023-07-22 15:39:35 +0100

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,20 @@
+const key = "SpYB4zOBi9f4yPeIwGtgB7IUNAXxkGAV";
+
+getCity = async (city) =>{
+    const base = "https://dataservice.accuweather.com/locations/v1/cities/search"
+    const query = `?apikey=${key}&q=${city}`;
+    const request = await fetch(base+query);
+    const data  =  await request.json();
+    return data[0];
+}
+
+getWeather = async (id) =>{
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/"
+    const query = `${id}?apikey=${key}`;
+    const request = await fetch(base+query);
+    const data  =  await request.json();
+    return data[0]; 
+}
+
+
+
